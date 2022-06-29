5642f56842cebf58ddea1bcd0009e5d35e8537f2
william <william.hm.w@hotmail.com>
2022-06-29 11:03:57 -0300

    App to-do full

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { ItemWrapper } from '../components/ItemWrapper';
@@ -11,17 +11,27 @@ interface Task{
   title: string;
   done: boolean;
 }
+export type EditTaskArgs = {
+  taskId: number;
+  taskNewTitle: string;
+}
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
   
   function handleAddTask(newTaskTitle: string) {
+    const taskWithSameTitle = tasks.find(task => task.title === newTaskTitle)
+
+    if(taskWithSameTitle){
+      return Alert.alert('Task já cadastrada', 'Você não pode cadastrar uma task com o mesmo nome')
+    }
+
     const newTask={
       id: Number(new Date().getTime()),
       title: newTaskTitle,
       done: false
     }
-    //TODO - add new task
+
     setTasks(oldTasks => [...oldTasks, newTask]);
   }
 
@@ -38,9 +48,34 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    const updatedTask = tasks.filter(task => task.id !== id);
+    Alert.alert('Remover item','Tem certeza que você deseja remover esse To-DO?',[
+      {
+        style: 'cancel',
+        text: 'Não'
+      },
+      {
+        style: 'destructive',
+        text: 'sim',
+        onPress: () => {
+          const updateTasks = tasks.filter(task => task.id !== id);
 
-    setTasks(updatedTask);
+          setTasks(updateTasks);
+        }
+      }
+    ])
+  }
+
+  function handleEditTask({taskId, taskNewTitle}: EditTaskArgs){
+    const updatedTasks = tasks.map(task => ({...task}))
+
+    const taskToBeUpdate = updatedTasks.find(task => task.id === task.id);
+
+    if(!taskToBeUpdate)
+    return;
+
+    taskToBeUpdate.title = taskNewTitle;
+
+    setTasks(updatedTasks);
   }
 
   return (
@@ -53,6 +88,7 @@ export function Home() {
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
+        editTask={handleEditTask}
       />
     </View>
   )
